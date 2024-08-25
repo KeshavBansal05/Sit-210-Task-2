bcd0fd26dced34ea6ea36918245dae317b82c7c9
KeshavBansal05 <keshav4788.be23@chitkara.edu.in>
2024-08-25 14:03:32 +0530

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Bansal"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "bansaljikeshavji"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2635766			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "YQE0IEKVSRJCE398"   // replace XYZ with your channel write API Key
